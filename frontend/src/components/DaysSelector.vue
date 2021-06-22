0d73f5dc00610010da7182022e5b1b8f8c66c64f
1n9yun <jig7357@gmail.com>
2021-06-22 23:18:40 +0900

    add daysSelector

---
+++ frontend/src/components/DaysSelector.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-row>
+        <v-col
+        cols="12"
+        >
+            <v-btn-toggle
+            :value="days"
+            @change="$emit('update:days', $event)"
+            multiple
+            rounded
+            style="width:100%;"
+            >
+                <v-btn
+                v-for="day in ['일','월','화','수','목','금','토']"
+                :key="day"
+                :width="100 / 7 + '%'"
+                >
+                    {{day}}
+                </v-btn>
+            </v-btn-toggle>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+    props: [
+        'days',
+    ],
+};
+</script>
+
+<style>
+</style>
